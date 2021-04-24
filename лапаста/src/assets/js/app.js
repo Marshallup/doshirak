41adf4ddd981feeb880f8e9de115e586aec4b5b2
Mikhail Teslya <televonvea@gmail.com>
2021-04-24 12:14:45 +0300

    asdasd

---
+++ лапаста/src/assets/js/app.js
@@ -0,0 +1,11 @@
+//= ./tiny-slider.js
+
+var slider = tns({
+  container: '.lapasta-about-text-slider-video-slider',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  prevButton: '.lapasta-about-text-slider-video-slider-controlls__prev',
+  nextButton: '.lapasta-about-text-slider-video-slider-controlls__next',
+  nav: false
+});
---
+++ чан рамен/src/assets/js/app.js
@@ -0,0 +1,22 @@
+//= ./tiny-slider.js
+
+var sliderAbout = tns({
+  container: '.chan-ramen-type-about-slider',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  prevButton: '.chan-ramen-type-about-slider__prev',
+  nextButton: '.chan-ramen-type-about-slider__next',
+  nav: false,
+  autoHeight: true
+});
+var sliderChazhang = tns({
+  container: '.chan-ramen-type-chazhang-slider',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  prevButton: '.chan-ramen-type-chazhang-slider__prev',
+  nextButton: '.chan-ramen-type-chazhang-slider__next',
+  nav: false,
+  autoHeight: true
+});
